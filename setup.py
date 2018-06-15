bb3d9e9bf00c76ff1b4523a1128a96834a70ca99
betteridiot <mdsherman@betteridiot.tech>
2018-06-15 14:11:37 -0400

    more stuff

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='bamnostic',
-    version='0.8.4b5',
+    version='0.8.5',
     description='Pure Python, OS-agnostic Binary Alignment Map (BAM) random access and parsing tool',
     long_description=readme(),
     url='https://github.com/betteridiot/bamnostic/',
