97224853d5dc710224f74e82f516d09c64074c09
betteridiot <mdsherman@betteridiot.tech>
2018-09-07 16:16:08 -0400

    debugging CI

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,10 +6,15 @@ git_config() {
 }
 
 git_push() {
+    echo "Adding Remote"
     git remote add origin https://${GH_TOKEN}@github.com/betteridiot/bamnostic.git > /dev/null 2>&1
+    echo "Fetching remote branch: devel"
     git branch --track dev origin/devel
+    git branch
     git merge devel
+    echo "Successful Merge"
     git push --quiet --set-upstream origin master
+    echo "Push Complete"
 }
 
 git_config
