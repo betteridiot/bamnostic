6e755a70c4348ee00bdcc953671e7020cc381adc
betteridiot <mdsherman@betteridiot.tech>
2018-09-09 14:45:15 -0400

    typo in travis after_success script

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,7 +9,7 @@ git_push() {
     echo "Adding Remote"
     git remote add origin https://${GH_TOKEN}@github.com/betteridiot/bamnostic.git > /dev/null 2>&1
     echo "Fetching remote branch: devel"
-    git branch --track dev origin/devel
+    git branch --track devel origin/devel
     git branch
     git merge devel
     echo "Successful Merge"
