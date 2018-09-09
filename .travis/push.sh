7fcacdac685d2861902f0c06739ddbfcd3467a4f
betteridiot <mdsherman@betteridiot.tech>
2018-09-09 16:37:23 -0400

    another hoorah

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,16 +9,7 @@ git_push() {
     git remote -v
     echo "Adding Remote"
     git remote add newOrigin https://${GH_TOKEN}@github.com/betteridiot/bamnostic.git
-    echo "Fetching origin"
-    git fetch newOrigin
-    echo "Branches"
-    git branch -vv
-    echo "Checking out master"
-    git checkout -b om newOrigin/master
-    echo "Attempting merge"
-    git merge newOrigin/devel
-    echo "Attempting push"
-    git push om
+     git push om HEAD:master
 }
 
 git_config
