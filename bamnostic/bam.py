cbb5f056bea8079e4cbf65d90e09958bb9db57be
betteridiot <mdsherman@betteridiot.tech>
2018-09-20 10:13:06 -0400

    Updating pytests

--- bamnostic/bam.py
+++ bamnostic/bam.py
@@ -520,7 +520,7 @@ class BamReader(bgzf.BgzfReader):
 
         Examples:
             >>> bam = bamnostic.AlignmentFile(bamnostic.example_bam, 'rb')
-            >>> next(bam.fetch('chr1', 1, 10)) # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+            >>> next(bam.fetch('chr1', 1, 100)) # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
             EAS56_57:6:190:289:82 ... MF:C:192
 
             >>> next(bam.fetch('chr10', 1, 10))
@@ -660,10 +660,10 @@ class BamReader(bgzf.BgzfReader):
         Example:
             >>> bam = bamnostic.AlignmentFile(bamnostic.example_bam, 'rb')
             >>> bam.count('chr1', 1, 100)
-            3
+            2
 
             >>> bam.count('chr1', 1, 100, read_callback='all')
-            2
+            1
 
             >>> bam.count('chr10', 1, 10)
             Traceback (most recent call last):
@@ -758,14 +758,14 @@ class BamReader(bgzf.BgzfReader):
             ...     print("array('{}', {})".format(arr.typecode, list(map(int, arr.tolist()))))
             array('L', [0, 0, 0, 0, ..., 0, 0, 0, 0, 0])
             array('L', [0, 0, 0, 0, ..., 0, 0, 0, 0, 0])
-            array('L', [1, 1, 2, 2, ..., 0, 14, 0, 14, 14])
+            array('L', [0, 0, 1, 1, ..., 0, 14, 0, 14, 14])
             array('L', [0, 0, 0, 0, ..., 15, 0, 14, 0, 0])
 
             >>> for arr in bam.count_coverage('chr1', 100, 150, quality_threshold=20, base_quality_threshold=25): # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
             ...     print("array('{}', {})".format(arr.typecode, list(map(int, arr.tolist()))))
             array('L', [0, 0, 0, 0, ..., 0, 0, 0, 0, 0])
             array('L', [0, 0, 0, 0, ..., 0, 0, 0, 0, 0])
-            array('L', [1, 1, 2, 2, ..., 0, 14, 0, 13, 11])
+            array('L', [0, 0, 1, 1, ..., 0, 14, 0, 13, 11])
             array('L', [0, 0, 0, 0, ..., 14, 0, 13, 0, 0])
 
         """
--- tests/test_build.py
+++ tests/test_build.py
@@ -13,3 +13,14 @@ def test_first_read():
     with bs.AlignmentFile(bs.example_bam, 'rb') as bam:
         first_read = next(bam)
         assert first_read.read_name == 'EAS56_57:6:190:289:82'
+
+
+def test_check_index():
+    with bs.AlignmentFile(bs.example_bam) as bam:
+        with pytest.warns(UserWarning):
+            bam.check_index('not_a_file.bai')
+
+
+def test_get_index():
+    with pytest.warns(UserWarning):
+        bam_no_bai = bs.AlignmentFile(bs.example_bam, index_filename='not_a_file.bai')
