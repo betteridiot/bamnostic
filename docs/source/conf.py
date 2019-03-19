6e008626e631ea6b929550dd1c2aacf0ffe1c070
Marcus D Sherman <betteridiot@users.noreply.github.com>
2019-03-19 14:44:10 -0400

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Marcus D. Sherman'
 author = 'Marcus D. Sherman'
 
 # The short X.Y version
-version = '1.0.7'
+version = '1.0.8'
 # The full version, including alpha/beta/rc tags
-release = '1.0.7'
+release = '1.0.8'
 
 
 # -- General configuration ---------------------------------------------------
